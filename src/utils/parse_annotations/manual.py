8a938f71c415e965d1b363feaa12f148726aa50d
mlubneuskaya <mlubneuskaya@gmail.com>
2026-04-02 15:38:55 +0200

    -

--- src/utils/parse_annotations/manual.py
+++ src/utils/parse_annotations/manual.py
@@ -4,10 +4,12 @@ import pandas as pd
 def parse_annotations(file_path):
     df = pd.read_csv(file_path, names=["frame", "event_id", "label"], header=0)
     df = df[df["label"] != "Neutral"].copy()
-    df['side'] = df['label'].apply(lambda x: 'right' if 'RIGHT' in x.upper() else 'left')
+    df["side"] = df["label"].apply(
+        lambda x: "right" if "RIGHT" in x.upper() else "left"
+    )
 
-    df['event_type'] = df['label'].apply(
-        lambda x: x.split('(')[-1].replace(')', '').strip() if '(' in x else x
+    df["event_type"] = df["label"].apply(
+        lambda x: x.split("(")[-1].replace(")", "").strip() if "(" in x else x
     )
 
     return df
